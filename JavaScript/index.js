a1861d26fb54c37a864b3d3043f2fc1ee1e5247f
Valmir-Neto <netovalmirr@gmail.com>
2025-05-11 18:33:59 -0300

    lógica de fazer aparecer as telas finalizadas

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -23,40 +23,55 @@ function abrirDrop() {
 
 btnAbrirDrop.addEventListener("click", abrirDrop)
 
-const about = document.getElementById("about")
-const realismo = document.getElementById("realismo")
-const line = document.getElementById("line")
-const old = document.getElementById("old")
-const interface = document.getElementById("interface")
+const about = document.getElementById("about");
+const realismo = document.getElementById("realismo");
+const line = document.getElementById("line");
+const old = document.getElementById("old");
+const interface = document.getElementById("interface");
+// limpa a classe selecionado de todos os itens para garantir que nenhum esteja com ela
+function limparSelecionados() {
+    about.classList.remove("selecionado");
+    realismo.classList.remove("selecionado");
+    line.classList.remove("selecionado");
+    old.classList.remove("selecionado");
+}
+// limpa as classes referentes as seções para garantir que esteja sem elas
+function limparInterface() {
+    interface.classList.remove("sobre")
+    interface.classList.remove("tattoLine")
+    interface.classList.remove("tattoRealismo")
+    interface.classList.remove("tattoOld")
+}
 
 function abrirAbout() {
-    interface.className = '';
-    about.classList.add("selecionado")
-    realismo.className = '';
-    line.className = '';
-    old.className = '';
-    interface.classList.add("sobre")
-}
-function abrirRealismo() {
-    about.className = '';
-    realismo.classList.add("selecionado")
-    line.className = '';
-    old.className = '';
-}
-function abrirLine() {
-     about.className = '';
-    realismo.className = '';
-    line.classList.add("selecionado")
-    old.className = '';
-}
-function abrirOld() {
-     about.className = '';
-    realismo.className = '';
-    line.className = '';
-    old.classList.add("selecionado")
+    limparSelecionados();
+    // limparInterface();
+    about.classList.add("selecionado");
+    // interface.classList.add("sobre");
 }
 
-about.addEventListener("click", abrirAbout)
-realismo.addEventListener("click", abrirRealismo)
-line.addEventListener("click", abrirLine)
-old.addEventListener("click", abrirOld)
+function abrirRealismo() {
+    // limparInterface();
+    limparSelecionados();
+    realismo.classList.add("selecionado");
+    // interface.classList.add("tattoRealismo");
+}
+
+function abrirLine() {
+    limparSelecionados();
+    // limparInterface();
+    line.classList.add("selecionado");
+    // interface.classList.add("tattoLine");
+}
+
+function abrirOld() {
+    // limparInterface();
+    limparSelecionados();
+    old.classList.add("selecionado");
+    // interface.classList.remove("tattoOld");
+}
+
+about.addEventListener("click", abrirAbout);
+realismo.addEventListener("click", abrirRealismo);
+line.addEventListener("click", abrirLine);
+old.addEventListener("click", abrirOld);
