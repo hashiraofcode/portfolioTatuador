64db25de73159c4ffdee915c62abe1da849cef2a
Valmir-Neto <netovalmirr@gmail.com>
2025-05-11 18:04:03 -0300

    lógica de estilização do  botão

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -22,3 +22,41 @@ function abrirDrop() {
 }
 
 btnAbrirDrop.addEventListener("click", abrirDrop)
+
+const about = document.getElementById("about")
+const realismo = document.getElementById("realismo")
+const line = document.getElementById("line")
+const old = document.getElementById("old")
+const interface = document.getElementById("interface")
+
+function abrirAbout() {
+    interface.className = '';
+    about.classList.add("selecionado")
+    realismo.className = '';
+    line.className = '';
+    old.className = '';
+    interface.classList.add("sobre")
+}
+function abrirRealismo() {
+    about.className = '';
+    realismo.classList.add("selecionado")
+    line.className = '';
+    old.className = '';
+}
+function abrirLine() {
+     about.className = '';
+    realismo.className = '';
+    line.classList.add("selecionado")
+    old.className = '';
+}
+function abrirOld() {
+     about.className = '';
+    realismo.className = '';
+    line.className = '';
+    old.classList.add("selecionado")
+}
+
+about.addEventListener("click", abrirAbout)
+realismo.addEventListener("click", abrirRealismo)
+line.addEventListener("click", abrirLine)
+old.addEventListener("click", abrirOld)
